ff2b41b240481674f8f0c6b382d9c019b8d0a235
Toni Cafiero <tcafiero@hotmail.com>
2020-12-15 19:45:25 +0100

    work in progress

--- src/pageCembalo.svelte
+++ src/pageCembalo.svelte
@@ -1,18 +1,18 @@
 <script>
-	let days = 3;
 	let chartTemp = 1;
 	let chartHum = 2;
 	let titleTemp = "Sala Cembalo - Temperature";
 	let titleHum = "Sala Cembalo - Humidity";
+	import {days} from './stores';
 </script>
 
 <section id="PlotSection" class="visible">
 <h1>MUSA Monitoring</h1>
 <label>Days</label>
-<input type=number bind:value={days} min=0 max=100>
+<input type=number bind:value={$days} min=0 max=100>
 
 <div id="chart1">
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartTemp}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleTemp}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartHum}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleHum}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
+<iframe width="350" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartTemp}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleTemp}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
+<iframe width="350" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartHum}?bgcolor=%23ffffff&color=%230033cc&days={days}&dynamic=true&title={titleHum}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%25"></iframe>
 </div>
 </section>
--- src/pageMostra.svelte
+++ src/pageMostra.svelte
@@ -1,17 +1,18 @@
 <script>
-	let days = 3;
 	let chartTemp = 7;
 	let chartHum = 8;
 	let titleTemp = "Sala Mostra - Temperature";
 	let titleHum = "Sala Mostra - Humidity";
+	import {days} from './stores';
 </script>
 
 <section id="PlotSection" class="visible">
 <h1>MUSA Monitoring</h1>
 <label>Days</label>
-<input type=number bind:value={days} min=0 max=100>
-<div id="chart">
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartTemp}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleTemp}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
-<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartHum}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleHum}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
+<input type=number bind:value={$days} min=0 max=100>
+
+<div id="chart1">
+<iframe width="350" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartTemp}?bgcolor=%23ffffff&color=%23d62020&days={days}&dynamic=true&title={titleTemp}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%C2%B0C"></iframe>
+<iframe width="350" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/505552/charts/{chartHum}?bgcolor=%23ffffff&color=%230033cc&days={days}&dynamic=true&title={titleHum}&type=spline&update=1&xaxis=hh%3Amm&yaxis=%25"></iframe>
 </div>
 </section>
